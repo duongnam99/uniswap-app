9e58858f463f6e1cf7e7cd8392a3af3ff7275122
Minh Nam <duongminhnam99@gmail.com>
2022-03-11 19:46:05 +0700

    [Update] css for swap page

--- client/pages/index.js
+++ client/pages/index.js
@@ -4,7 +4,7 @@ import Header from '../components/Header'
 import Main from '../components/Main'
 
 const style = {
-  wrapper: `h-screen max-h-screen h-min-screen w-screen bg-[#2d3436] text-white select-none flex flex-col justify-between`,
+  wrapper: `h-screen max-h-screen h-min-screen w-screen bg-[#2d3436] text-white select-none flex flex-col`,
 }
 const Home = () => {
   return (
