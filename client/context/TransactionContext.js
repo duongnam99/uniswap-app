ea2518eb4db669ed7e499a99e8ae91218c0cfe46
Minh Nam <duongminhnam99@gmail.com>
2022-03-12 14:35:11 +0700

    [Update] publicTransaction feature

--- client/context/TransactionContext.js
+++ client/context/TransactionContext.js
@@ -10,13 +10,17 @@ if (typeof window !== 'undefined') { // prevent window undefined error
 
 export const TransactionProvider = ({children}) => {
     const [currentAccount, setCurrentAccount] = useState()
+    const [isLoading, setIsLoading] = useState(false)
+    const [formData, setFormData] = useState({
+        addressTo: '',
+        amount: '',
+    })
 
     useEffect(() => {
         checkIfWalletIsConnected()
     }, [])
 
     const connectWallet = async (metamask = eth) => {
-        console.log(metamask)
         try {
             if (!metamask) return alert("You must install metamask first!!!")
             const accounts = await metamask.request({method: 'eth_requestAccounts'})
@@ -42,11 +46,61 @@ export const TransactionProvider = ({children}) => {
         }
     }
 
+    const sendTransaction = async (
+        metamask = eth,
+        connectedAccount = currentAccount
+    ) => {
+        try {
+            if (!metamask) return alert("Please install metamaks")
+            const {addressTo, amount} = formData
+            const transactionContract = getEthereumcontract()
+            const parsedAmount = ethers.utils.parseEther(amount)
+
+            await metamask.requrest({
+                method: 'eth_sendTransaction',
+                params: [
+                    {
+                        from: connectedAccount,
+                        to: addressTo,
+                        gas: 0x7E40, // 520000wei
+                        value: parsedAmount._hex
+                    },
+                ],
+            })
+
+            const transactionHash = await transactionContract.publishTransaction(
+                addressTo,
+                parsedAmount,
+                `Transfering ETH ${parsedAmount} to ${addressTo}`,
+                'TRANSFER'
+            )
+
+            setIsLoading(true)
+
+            await transactionHash.wait()
+            
+            // DB
+            // await saveTransaction(
+            //     transactionHash.hash,
+            //     amount,
+            //     connectedAccount,
+            //     addressTo
+            // )
+            setIsLoading(false)
+
+        } catch (error) {
+            console.error(error)
+        }
+    }
+    const handleChange = (e, name) => {
+        setFormData((prevState) => ({...prevState, [name]: e.target.value}))
+    }
     return (
         <TransactionContext.Provider
             value={{
                 currentAccount,
                 connectWallet,
+                sendTransaction,
             }}
         >
             {children}
